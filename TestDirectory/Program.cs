9d0afc9c12532d3f924505324c82f89d2b480487
Denys <78100050+denyslysohor@users.noreply.github.com>
2021-11-26 11:24:36 +0200

    New changes 11/26/2021
    New exercises and challenges.

--- TestDirectory/Program.cs
+++ TestDirectory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static ConsoleTestApp.FuncDif;
 using static ConsoleTestApp.StructTypes;
 
 namespace ConsoleTestApp 
@@ -14,13 +15,196 @@ namespace ConsoleTestApp
 
             return ("", "", 0);
         }
+
+        // Action delegate
+
+        static void Operation(int x1, int x2, Action<int, int> op)
+        {
+            if (x1 != x2)
+                op(x1, x2);
+        }
+
+        static void Add(int x1, int x2)
+        {
+            Console.WriteLine("Сумма чисел: " + (x1 + x2));
+        }
+
+        static void Substract(int x1, int x2)
+        {
+            Console.WriteLine("Разность чисел: " + (x1 - x2));
+        }
+
+        static void Multiply(int x1, int x2)
+        {
+            Console.WriteLine("Произведение чисел: " + (x1 * x2));
+        }
+
         public static void Main(string[] args)
         {
+            // Challenge PARTIAL Classes
+
+            SubSystem subSystem = new("Sub1", 40);
+            SubSystem subSystem1 = new("John Connor");
+            subSystem.DisplayInfo();
+
+            // Searching in strings
+
+            string message = "Find what is (inside the parentheses)";
+
+            int openingPosition = message.IndexOf('(');
+            int closingPosition = message.IndexOf(')');
+
+            int length = closingPosition - openingPosition;
+            Console.WriteLine(message.Substring(openingPosition + 1, length - 1));
+            Console.WriteLine();
+
+            string message1 = "This--is--ex-amp-le--da-ta";
+            message1 = message1.Replace("--", " ");
+            message1 = message1.Replace("-", "");
+            Console.WriteLine(message1);
+            Console.WriteLine();
+
+            const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+            string quantity = "";
+            string output = "";
+
+            string spanTagOpen = "<span>";
+            string spanTagClose = "</span>";
+
+            int quantityStart = input.IndexOf(spanTagOpen);
+            int quantityEnd = input.IndexOf(spanTagClose);
+
+            quantityStart += spanTagOpen.Length;
+            int quantityLength = quantityEnd - quantityStart;
+            quantity = input.Substring(quantityStart, quantityLength);
+
+            output = input.Replace("&trade;", "&reg;");
+
+            int divStart = input.IndexOf("<div");
+            int divEnd = input.IndexOf(">");
+            int divLength = divEnd - divStart;
+            divLength += 1;
+            output = output.Remove(divStart, divLength);
+
+            int divCloseStart = output.IndexOf("</div");
+            int divCloseEnd = output.IndexOf(">", divCloseStart);
+            int divCloseLength = divCloseEnd - divCloseStart;
+            divCloseLength += 1;
+            output = output.Remove(divCloseStart, divCloseLength);
+
+            Console.WriteLine(quantity);
+            Console.WriteLine(output);
+
+            // Challenge of data formatting, from MS Learn
+            string customerName = "Mr. Jones";
+
+            string currentProduct = "Magic Yield";
+            int currentShares = 23123;
+            decimal currentReturn = 0.12322m;
+            decimal currentProfit = 123000.0m;
+
+            string newProduct = "Glorious Future";
+            decimal newReturn = 0.13125m;
+            decimal newProfit = 63000000.0m;
+
+            Console.WriteLine($"Dear {customerName},");
+            Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+            Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+            Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+            Console.WriteLine("Here's a quick comparison:\n");
+
+            string comparisonMessage = "";
+
+            comparisonMessage = currentProduct.PadRight(20);
+
+            comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+            comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+            comparisonMessage += "\n";
+
+            comparisonMessage += newProduct.PadRight(20);
+
+            comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+            comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+            Console.WriteLine(comparisonMessage);
+
+            // Challenge of using action delegate
+            Action<int, int> op;
+            op = Add;
+            Operation(10, 6, op);
+            op = Substract;
+            Operation(10, 6, op);
+            op = Multiply;
+            Operation(8, 10, op);
+            Operation(8, 8, op);
+
+            // The instance of base String Formatting
+
+            Console.WriteLine();
+            string firstString = "Hello";
+            string secondString = "World";
+            string resultString = string.Format("{0} {1}!", firstString, secondString);
+            Console.WriteLine(resultString);
+
+            // OR
+
+            Console.WriteLine("{0} {1}!", firstString, secondString);
+
+            // Using interpolation
+
+            Console.WriteLine($"{firstString} {secondString}!");
+
+
+            // Currency formatting
+
+            decimal price = 123.45m;
+            int discount = 50;
+            Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+            // ':C' - means the currency token of the country. USA - $, etc.
+
+
+            // Percentages formatting
+
+            decimal tax = .2298767m;
+            Console.WriteLine($"Tax rate: {tax:P2}"); // Output: 22,99%
+
+
+            // Small Challenge of FORMATTING
+
+            Console.WriteLine();
+            int invoiceNumber = 1201;
+            decimal productMeasurement = 25.4568m;
+            decimal subtotal = 2750.00m;
+            decimal taxPercentage = .15825m;
+            decimal total = 3185.19m;
+
+            Console.WriteLine($"Invoice Number: {invoiceNumber}");
+            Console.WriteLine($"\tMeasurement: {productMeasurement:N3} mg");
+            Console.WriteLine($"\t\tSub Total: {subtotal:C}");
+            Console.WriteLine($"\t\t\tTax: {taxPercentage:P2}");
+            Console.WriteLine($"\t\t\t\tTotal Due: {total:C}");
+
+
+            // The common and anonymous methods
+
+            Console.WriteLine();
+            double result1 = MultipleFunc(4, 5);
+
+            Console.WriteLine($"{result1}");
+
+            MultipleDelegate multipleDelegate = new(FuncDif.Multiple);
+            double result2 = multipleDelegate.Invoke(7, 5);
+            Console.WriteLine(result2);
+
+
+            // Local Machine Parameters
 
             LocalMachine machine = new LocalMachine();
             machine.GetMachineParameters();
-
-
 
 
             // Tuple Pattern Matching
@@ -53,22 +237,6 @@ namespace ConsoleTestApp
             var year = result.Item3;
 
             Console.WriteLine(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
             // IO namespace some tests:
---
+++ TestDirectory/SubSystem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestApp
+{
+    public partial class SubSystem
+    {
+        public string nameOfSubSystem;
+        public int countOfGroups;
+
+        public SubSystem(string name, int count)
+        {
+            nameOfSubSystem = name;
+            countOfGroups = count;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name of Current SubSystem: {nameOfSubSystem}");
+            Console.WriteLine($"Total count of groups in {nameOfSubSystem} - {countOfGroups}");
+            Console.WriteLine($"Current owner: {currentOwner}");
+        }
+    }
+}
